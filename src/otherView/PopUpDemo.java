661a15afd5879885a796c9f555554f8b0c221f4f
rrokgjinaj <rrok.gjinaj@studio.unibo.it>
2016-08-02 09:31:15 +0200

    addel select to palylist

--- src/otherView/PopUpDemo.java
+++ src/otherView/PopUpDemo.java
@@ -3,8 +3,11 @@ package otherView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+
+
 
 public class PopUpDemo extends JPopupMenu{
     /**
@@ -12,7 +15,10 @@ public class PopUpDemo extends JPopupMenu{
 	 */
 	private static final long serialVersionUID = 1L;
 	JMenuItem remove;
-	JMenuItem aggiungiaPlaylist;
+	JMenuItem salva_nei_preferiti;
+	JMenu sectionsMenu = new JMenu("Aggiungi Alla Playlist");
+	JMenuItem menuItem1 = new JMenuItem("Playlist 1");
+	JMenuItem menuItem2 = new JMenuItem("Playlist 2");
 	
     public PopUpDemo(){
     	remove = new JMenuItem("remove");
@@ -25,16 +31,16 @@ public class PopUpDemo extends JPopupMenu{
 				
 			}
 		});
-    	aggiungiaPlaylist= new JMenuItem("AddToPlaylist");
-    	add(aggiungiaPlaylist);
-        add(remove);
-        PopUpDemo p= new PopUpDemo();
-        
-        JMenuItem remove = new JMenuItem("cc");
-        p.add(remove);
-        this.add(p);
-        
-        
+    	//sub menu
+    	
+    	this.add(remove);
+    	this.add(remove);
+    	
+    	sectionsMenu.add(menuItem1 );
+
+    	sectionsMenu.add(menuItem2 );
+    	
+    	this.add(sectionsMenu);
     }
 
 }
